949335ca5691e0df0fb4442c8b6ab45d3e4677d8
Jozef Butko <jbutko@gmail.com>
2014-03-07 23:11:52 +0100

    initial commit v1.0.0

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function () {
+  
+  $('.header').stickyNavbar({
+    activeClass: "active",
+    sectionSelector: "scrollto",
+    navOffset: 0,
+    animDuration: 300,
+    startAt: 0, // Stick the menu at XXXpx from the top
+    easing: "easeInQuad",
+    bottomAnimation: true,
+    jqueryEffects: false,
+    animateCSS: true,
+    animateCSSRepeat: false,
+    selector: "a",
+    jqueryAnim: "fadeInDown" // jQuery effects: fadeIn, show, slideDown
+  });
+});
+
+SyntaxHighlighter.config.bloggerMode = true;
+SyntaxHighlighter.config.clipboardSwf = 'http://alexgorbatchev.com/pub/sh/current/scripts/clipboard.swf';
+SyntaxHighlighter.all();
